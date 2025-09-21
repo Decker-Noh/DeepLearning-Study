62284bb956019528121c56ae91c103afbd4c47f5
Decker-Noh <76258933+Decker-Noh@users.noreply.github.com>
2025-09-21 23:08:27 +0900

    RMSE

---
+++ RMSE.py
@@ -0,0 +1,24 @@
+import numpy as np
+# 임의값
+ab = [3, 76]
+
+data = [[2, 81], [4, 93], [6, 91], [8, 97]]
+x= [i[0] for i in data]
+y= [i[1] for i in data]
+
+def predict(x):
+    return ab[0]*x + ab[1]
+
+def rmse(p, a):
+    return np.sqrt(((p-a)**2).mean())
+
+def rmse_val(predict_result, y):
+    return rmse(np.array(predict_result), np.array(y))
+
+predict_result = []
+
+for i in range(len(x)):
+    predict_result.append(predict(x[i]))
+    print("실제x : %.f, 실제y : %.f, 예측y : %.f" % (x[i], y[i], predict(x[i])))
+
+print("최종 : ", str(rmse_val(predict_result, y)))
